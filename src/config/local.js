6cb6ab4df85a0d49bacbf35ffe6decb9cd889044
Maiky Nata <maikynata25@gmail.com>
2019-05-07 17:02:59 -0300

    initialZoom and initialView changes

--- src/config/local.js
+++ src/config/local.js
@@ -1,6 +1,6 @@
 module.exports = {
-  environment: "production",
-  uploadBucket: "oam-uploader-production-temp",
+environment: "local",
+  uploadBucket: "oam-uploader-local-temp",
   googleClient:
     "36015894456-cln93odnr88523ssjkaaf8km7fi0snos.apps.googleusercontent.com",
   googleDeveloperKey: "",
@@ -9,10 +9,12 @@ module.exports = {
       accessToken:
         "pk.eyJ1Ijoib3BlbmFlcmlhbG1hcCIsImEiOiJjaXl4MjM5c20wMDBmMzNucnZtbnYwZTcxIn0.IKG5flWCS6QfpO3iOdRveg"
     },
-    initialZoom: 3,
+
+    initialZoom: 8,
     minZoom: 2,
     maxZoom: 18,
-    initialView: [-51.48193359375,-15.029685756555674]
+
+    initialView: [-51.4819335,-15.0296857]
   },
   catalog: {
     url: "https://api.openaerialmap.org"
@@ -21,5 +23,4 @@ module.exports = {
   feedbackSubmissionURL:
     "https://getsimpleform.com/messages/ajax?form_api_token=506fc2ac58582416b6086a68a343e344",
   OAMBrowserUrl: "https://map.openaerialmap.org",
-  awsKey: "AKIAJJRD6S67FNI7SZWQ"
-};
+  awsKey: "AKIAJJRD6S67FNI7SZWQ"};
